7491139a738b8e1b15d67d41f42cc3f92da1a922
Kevin Williams <kevin.williams@vastech.co.za>
2024-01-31 08:29:44 +0200

    uhd 4.3

--- send_timed_command.py
+++ send_timed_command.py
@@ -1,4 +1,5 @@
 import uhd
+import uhd.types
 import numpy as np
 
 def main():
@@ -11,14 +12,21 @@ def main():
     # set usrp realtime clock (to 0 for now)
     usrp.set_time_now(uhd.types.TimeSpec(0.0))
 
-    # set the center frequency
-    usrp.set_tx_freq(2.45e9)
-    
     # create a simple sine wave signal
     samples = np.exp(1j * np.pi * np.arange(4000))
 
+    # some constants
+    tx_duration = 10
+    center_freq = 2.45e9
+    sample_rate = 10e6
+    channels = [0]
+    tx_gain = 0
+    tx_time = uhd.types.TimeSpec(2)
+
     # send the signal
-    usrp.send_waveform(samples, uhd.types.TimeSpec(2))
+    # UHD provides the send_waveform() function to transmit a batch of samples, an example is shown below. 
+    # If you specify a duration (in seconds) longer than the provided signal, it will simply repeat it.
+    usrp.send_waveform(samples, tx_duration, center_freq, sample_rate, channels, tx_gain, tx_time)
 
 
 if __name__ == "__main__":
